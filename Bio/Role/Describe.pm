f232056b9cc04fed74523a7f6451be9e91c2acb3
cjfields <cjfields@bioperl.org>
2009-06-08 10:42:18 -0500

    * Should be Str

--- Bio/Role/Describe.pm
+++ Bio/Role/Describe.pm
@@ -3,5 +3,6 @@ role Bio::Role::Describe;
 # these are the primary data available that are similar to the BioPerl
 # Bio::DescribableI interface.
 
-has $.display_name              is rw;
-has $.description               is rw;
+has Str $.display_name              is rw;
+has Str $.description               is rw;
+
