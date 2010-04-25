f0e6240433bba4be14bd1e7fbb2a16a8706988d6
Takadonet <philipmabon@gmail.com>
2010-04-25 13:47:35 -0500

    Very simple version of write_seq method for Fasta files.

--- lib/Bio/Role/FastaIO.pm
+++ lib/Bio/Role/FastaIO.pm
@@ -1,5 +1,7 @@
 #stupid RT bug # 74078 Need to create new reference to see builtin functions
 our &shiftt = &shift;
+our &op = &open;
+our &s = &say;
 role Bio::Role::FastaIO[$file]{
 
 has @!results;
@@ -7,14 +9,36 @@ has $!fh;
 use Bio::Grammar::Fasta;
 use Bio::Grammar::Actions::Fasta;
  
- method next_seq() {
+method next_seq() {
   #rakudo bug, cannot use builtin functions in roles RT #74078
   return &shiftt(@!results);
 }
 
 #method will not be private and/or put in new buildmethod
-multi method initial() {    
+method initial_read() {    
  	@!results = @(Bio::Grammar::Fasta.parsefile($file, :actions(Bio::Grammar::Actions::Fasta)).ast);	
+}
+
+method initial_write() {    
+ 	#rakudo bug RT #74078
+ 	#need error checking to ensure that we did indeed open a new file
+ 	$!fh = &op($file,:w);
+}
+
+method write_seq(*@seq){
+ 
+ for @seq -> $seq {
+   #need to ensure we have Bio::PrimarySeqI
+   
+   #most raw way to print out fasta files with no options whatsoever
+   #no width of the sequence even!
+   my $header = ">$seq.display_id() $seq.description()";
+   my $sequence = $seq.seq();
+
+   $!fh.say($header ~ "\n" ~ $seq.seq());   
+   
+ }
+ 
 }
 
 }
--- lib/Bio/SeqIO.pm
+++ lib/Bio/SeqIO.pm
@@ -2,7 +2,7 @@ class Bio::SeqIO;
 
 has $!format;
 
-method new(Str $format,Str $file){
+method new(Str $format,Str $file,:$write?){
 	my Junction $formats = any ('fasta'|'genbank');
 	
 	my $obj;
@@ -11,7 +11,14 @@ method new(Str $format,Str $file){
 	if lc($format) eq 'fasta' {
 		use Bio::Role::FastaIO;
 		$obj = self.bless(*,:$format) does Bio::Role::FastaIO[$file];
-		$obj.initial();
+		
+		#should not have to do this......
+		if $write {
+			$obj.initial_write();
+		}
+		else {
+			$obj.initial_read();
+		}
 	}
 	else {
 		#need to use Bio::Root::Root
