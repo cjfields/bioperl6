c0196a87e8337ed48091aa83421556aa96a4d625
takadonet <philipmabon@gmail.com>
2011-03-06 21:55:40 -0600

    Added hashes with a new constructor to overcome attribute subtypes that are NYI. Passing more tests.

--- lib/Bio/Role/Location/Simple.pm
+++ lib/Bio/Role/Location/Simple.pm
@@ -23,9 +23,54 @@ has %!IS_FUZZY = map {;$_ => 1} , qw<BEFORE AFTER WITHIN UNCERTAIN>;
 has Int $.start_offset is rw = 0;
 has Int $.end_offset is rw = 0;
 
+#really want to get rid of all the hashes below
+#however cannot since subtype do not WORK with attributes!
+#strand switch since we allow '+' and '-'
+my %strands_switch = ('+' => 1 , '-' => -1);
+
+my %RANGEENCODE  = ('..' => 'EXACT',
+             '^'   => 'IN-BETWEEN' );
+
+my %RANGEDECODE  = ('EXACT'      => '..',
+             'IN-BETWEEN' => '^' );
+
+my %POSTYPEENCODE = ('<' => 'BEFORE',
+                     '>' => 'AFTER');
+
+my %POSTYPEDECODE = ('BEFORE'  => '<',
+                     'AFTER'   => '>');
+#########################################
+
+method new(*%params is copy){
+    
+    #parameter checking that should go away when subtypes work with attributes
+    #swapping out  '+' and '-' for integers in strand
+    if ( %strands_switch.exists(%params{'strand'})) {
+        %params{'strand'} = %strands_switch{%params{'strand'}};
+    }
+
+    #swapping out '..' and '^' for words in location_type
+    if ( %RANGEENCODE.exists(%params{'location_type'})) {
+        %params{'location_type'} = %RANGEENCODE{%params{'location_type'}};
+    }
+
+    #swapping out '<' and '>' for words in start/end pos type
+    if ( %POSTYPEENCODE.exists(%params{'start_pos_type'})) {
+        %params{'start_pos_type'} = %POSTYPEENCODE{%params{'start_pos_type'}};
+    }
+    if ( %POSTYPEENCODE.exists(%params{'end_pos_type'})) {
+        %params{'end_pos_type'} = %POSTYPEENCODE{%params{'end_pos_type'}};
+    }
+    
+    
+    my $x = self.bless(*,|%params);
+
+    return $x;
+}
+
+
 method to_string() {
     my %data;
-    say self.^attributes();
     for self.^attributes -> $x {
         %data{$x.name} = $x.get_value(self);
     }
@@ -52,10 +97,9 @@ method to_string() {
         %data{"$pos" ~  "_string"} = $pos_str;
     }
 
-    my $str = %data{'$!start_string'} ~  %data{'$!end_string'};        
-    # my $str = %data{'$!start_string'} ~  (%data{'$!end_string'} ?? 
-    #         self!to_Location_Symbol(%data{'$!location_type'}) ~ 
-    #         %data{'$!end_string'} !! '');
+    my $str = %data{'$!start_string'} ~  (%data{'$!end_string'} ?? 
+            %RANGEDECODE{%data{'$!location_type'}} ~ 
+            %data{'$!end_string'} !! '');    
     $str = "%data{'$!seq_id'}:$str" if %data{'$!seq_id'} && %data{'$!is_remote'};
     $str = "($str)" if %data{'$!location_type'} eq 'WITHIN';
     if (self.strand == -1) {
