c7d482ce650453e6c2c560d669c78e694a56c60e
takadonet <philipmabon@gmail.com>
2011-04-04 16:49:56 -0500

    Added attribute tagname and methods for StructuredValue.pm. Passing more tests

--- lib/Bio/Annotation/StructuredValue.pm
+++ lib/Bio/Annotation/StructuredValue.pm
@@ -60,13 +60,12 @@ class Bio::Annotation::StructuredValue is Bio::AnnotationI {
 
 # =cut
 
-# method tagname{
-#     my ($self,$value) = @_;
-#     if( defined $value) {
-# 	$self.{'tagname'} = $value;
-#     }
-#     return $self.{'tagname'};
-# }
+
+has $!tagname is rw;
+method tagname($value?) {
+    return $!tagname = $value if defined $value;
+    return $!tagname;
+}
 
 
 # =head1 Specific accessors for StructuredValue
